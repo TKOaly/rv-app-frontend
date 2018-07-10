88f8d1e452f3b8e7e4355964e867a43899e85417
Tuupertunut <superemppu@live.fi>
2018-07-11 00:29:20 +0300

    Cleaned ProductPopup.

--- src/components/modals/ProductPopup.js
+++ src/components/modals/ProductPopup.js
@@ -6,52 +6,58 @@ import React from 'react';
 import moneyFormatter from '../../services/moneyFormatter';
 
 class ProductPopup extends React.Component {
-    changeQuantity = (change) => {
-        if (this.props.buyAmount + change >= 1) {
-            this.props.setBuyAmount(this.props.buyAmount + change);
+    handleBuy = () => {
+        this.props.buyProduct(this.props.product, this.props.buyAmount);
+        this.props.closeModal();
+    };
+
+    handleCancel = () => {
+        this.props.closeModal();
+    };
+
+    handleDecreaseAmount = () => {
+        if (this.props.buyAmount > 1) {
+            this.props.setBuyAmount(this.props.buyAmount - 1);
         }
     };
 
+    handleIncreaseAmount = () => {
+        this.props.setBuyAmount(this.props.buyAmount + 1);
+    };
+
     componentWillUnmount = () => {
+        // Reset amount back to 1
         this.props.setBuyAmount(1);
     };
 
     render = () => {
-        const prod = this.props.product;
-
         return (
             <div className="product-popup">
                 <div className="product-popup-header">
                     <h3>Buy product</h3>
                 </div>
                 <div className="product-info">
-                    <div className="product-name">{prod.product_name}</div>
+                    <div className="product-name">{this.props.product.product_name}</div>
                 </div>
                 <div className="product-quantity">
                     <div>Amount:</div>
                     <div className="quantity-picker">
-                        <button onClick={() => this.changeQuantity(-1)}>-</button>
+                        <button onClick={this.handleDecreaseAmount}>-</button>
                         <span>{this.props.buyAmount} pcs</span>
-                        <button onClick={() => this.changeQuantity(1)}>+</button>
+                        <button onClick={this.handleIncreaseAmount}>+</button>
                     </div>
                     <div className="total">
                         <div>Total</div>
                         <div className="sum">
-                            {moneyFormatter.centsToString(prod.sellprice * this.props.buyAmount)} €
+                            {moneyFormatter.centsToString(this.props.product.sellprice * this.props.buyAmount)} €
                         </div>
                     </div>
                 </div>
                 <div className="product-actions">
-                    <button className="cancel-btn" onClick={() => this.props.closeModal()}>
+                    <button className="cancel-btn" onClick={this.handleCancel}>
                         Cancel
                     </button>
-                    <button
-                        className="purchase-btn"
-                        onClick={() => {
-                            this.props.buyProduct(prod, this.props.buyAmount);
-                            this.props.closeModal();
-                        }}
-                    >
+                    <button className="purchase-btn" onClick={this.handleBuy}>
                         Buy
                     </button>
                 </div>
