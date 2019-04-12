3289d13c58263908acddf2c3bb7e62efa7189f48
alehuo <aleksi.huotala@helsinki.fi>
2019-04-12 10:10:13 +0300

    Fix tests. Mock findDOMNode

--- src/components/pages/LoginPage.test.js
+++ src/components/pages/LoginPage.test.js
@@ -1,37 +1,49 @@
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { configure, shallow } from 'enzyme';
-import { store } from '../../store';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import LoginPage from './LoginPage';
 import React from 'react';
+import renderer from 'react-test-renderer';
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 /*
 Here we pass in the Redux store and a memory router,
  to test the functionality of the login page.
 */
 
+jest.mock('react-dom', () => ({
+    findDOMNode: () => ({
+        focus: () => {}
+    })
+}));
+
 describe('<LoginPage />', () => {
-    it('renders correctly when not authenticated', () => {
-        const cmpnt = shallow(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <LoginPage authenticated={false} />
-                </MemoryRouter>
-            </Provider>
-        );
+    it('renders correctly when not authenticated', async () => {
+        const { store } = await import('../../store');
+        const cmpnt = renderer
+            .create(
+                <Provider store={store}>
+                    <MemoryRouter>
+                        <LoginPage authenticated={false} />
+                    </MemoryRouter>
+                </Provider>
+            )
+            .toJSON();
         expect(cmpnt).toMatchSnapshot();
     });
-    it('renders correctly when authenticated', () => {
-        const cmpnt = shallow(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <LoginPage authenticated={true} />
-                </MemoryRouter>
-            </Provider>
-        );
+    it('renders correctly when authenticated', async () => {
+        const { store } = await import('../../store');
+        const cmpnt = renderer
+            .create(
+                <Provider store={store}>
+                    <MemoryRouter>
+                        <LoginPage authenticated={true} />
+                    </MemoryRouter>
+                </Provider>
+            )
+            .toJSON();
         expect(cmpnt).toMatchSnapshot();
     });
 });
