64cd2f9ae018349af60331c846db4c619c07421d
Tuupertunut <superemppu@live.fi>
2019-05-14 00:22:24 +0300

    Removed unused code from MainPage.

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,23 +1,15 @@
-import {
-    addProductToNotification,
-    clearProductsFromNotification,
-    errorMessage,
-    successMessage
-} from '../../reducers/notificationReducer';
+import { clearProductsFromNotification } from '../../reducers/notificationReducer';
 import { connect } from 'react-redux';
-import { decreaseBalance, increaseBalance, resetUserData } from '../../reducers/userReducer';
 import { getCategories, getProducts } from '../../reducers/productReducer';
 import { logout } from '../../reducers/authenticationReducer';
 import Content from '../sections/Content';
 import Header from '../sections/Header';
 import React from 'react';
-import userService from '../../services/userService';
 
 class MainPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            timeoutHandler: null,
             notificationInterval: null
         };
     }
@@ -41,45 +33,6 @@ class MainPage extends React.Component {
         this.setState({ notificationInterval: null });
     };
 
-    /**
-     * Buys a product.
-     */
-    buy = async (product) => {
-        try {
-            await userService.reduceBalance(this.props.token, product.price);
-
-            // If timeout is set, clear it to prevent notification from disappearing
-            if (this.state.timeoutHandler) {
-                clearTimeout(this.state.timeoutHandler);
-                this.setState({ timeoutHandler: null });
-            }
-
-            this.props.decreaseBalance(product.price);
-
-            // Add product to notification
-            this.props.addProductToNotification(product);
-
-            // Create a new timeout
-            const timeoutHandler = setTimeout(
-                () => this.props.clearProductsFromNotification(),
-                this.props.purchaseNotificationTimeout
-            );
-            this.setState({
-                timeoutHandler
-            });
-        } catch (error) {
-            const errorResponse = error.response;
-            this.props.errorMessage(errorResponse.data.message);
-        }
-    };
-
-    show = () => {
-        return (event) => {
-            event.preventDefault();
-            this.props.toggleVisibility(this.props.modalVisibility);
-        };
-    };
-
     render = () => {
         return (
             <div className="mainpage">
@@ -95,24 +48,18 @@ const mapStateToProps = (state) => {
         token: state.authentication.access_token,
         purchaseNotificationTimeout: state.notification.purchaseNotificationTimeout,
         purchaseNotificationStartTime: state.notification.purchaseNotificationStartTime,
-        user: state.user,
-        terminalInput: state.terminal.terminalInput,
-        filter: state.products.filter,
-        modalVisibility: state.modal.modalVisibility
+        user: state.user
     };
 };
 
 const mapDispatchToProps = {
-    successMessage,
-    errorMessage,
     logout,
-    addProductToNotification,
     clearProductsFromNotification,
-    increaseBalance,
-    decreaseBalance,
-    resetUserData,
     getProducts,
     getCategories
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(MainPage);
