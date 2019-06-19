7b19566e7db3dabd54654e72e97f2764b74e3f7f
Aleksi Huotala <aleksi.huotala@reaktor.com>
2019-06-19 20:06:25 +0300

    Add .nvmrc file with Node v12.4.0
    Tweak Deposit component style to fix issue with disappearing button texts. Switch to base font size and font scaling with child element font sizing
    
    Lint SCSS files
    
    Update package-lock.json
    
    Fix tests, refactor some components

--- src/_tests_/NotificationDrawer.test.js
+++ src/_tests_/NotificationDrawer.test.js
@@ -1,11 +1,19 @@
-import { shallow } from 'enzyme';
-import NotificationDrawer from '../components/helpers/NotificationDrawer';
+import { NotificationDrawer } from '../components/helpers/NotificationDrawer';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+// TODO: Move to mock file
+const mockNotifications = [
+    {
+        messageType: 'SUCCESS',
+        id: 1,
+        message: 'Hello World'
+    }
+];
+
 describe.only('<NotificationDrawer />', () => {
     it('renders correctly', () => {
-        const cmpnt = renderer.create(<NotificationDrawer />).toJSON();
+        const cmpnt = renderer.create(<NotificationDrawer notifications={mockNotifications}/>).toJSON();
         expect(cmpnt).toMatchSnapshot();
     });
 });
--- src/components/helpers/NotificationDrawer.js
+++ src/components/helpers/NotificationDrawer.js
@@ -7,34 +7,30 @@ import PurchaseNotification from '../notifications/PurchaseNotification';
 import React from 'react';
 import SuccessNotification from '../notifications/SuccessNotification';
 
-class NotificationDrawer extends React.Component {
-    render = () => {
-        return (
-            <div className="notificationDrawer">
-                <TransitionGroup>
-                    {this.props.notifications &&
-                        this.props.notifications.length > 0 &&
-                        this.props.notifications.map((notification, id) =>
-                            notification.messageType === 'SUCCESS' ? (
-                                <Fade key={notification.id}>
-                                    <SuccessNotification message={notification.message} shadow />
-                                </Fade>
-                            ) : (
-                                <Fade key={notification.id}>
-                                    <ErrorNotification message={notification.message} shadow />
-                                </Fade>
-                            )
-                        )}
-                    {this.props.purchases && this.props.purchases.length > 0 && (
-                        <Fade>
-                            <PurchaseNotification shadow purchases={this.props.purchases} />
-                        </Fade>
-                    )}
-                </TransitionGroup>
-            </div>
-        );
-    };
-}
+export const NotificationDrawer = ({ notifications, purchases }) => (
+    <div className="notificationDrawer">
+        <TransitionGroup>
+            {notifications &&
+        notifications.length > 0 &&
+        notifications.map((notification) =>
+            notification.messageType === 'SUCCESS' ? (
+                <Fade key={notification.id}>
+                    <SuccessNotification message={notification.message} shadow />
+                </Fade>
+            ) : (
+                <Fade key={notification.id}>
+                    <ErrorNotification message={notification.message} shadow />
+                </Fade>
+            )
+        )}
+            {purchases && purchases.length > 0 && (
+                <Fade>
+                    <PurchaseNotification shadow purchases={purchases} />
+                </Fade>
+            )}
+        </TransitionGroup>
+    </div>
+);
 
 const mapStateToProps = (state) => {
     return {
@@ -44,3 +40,4 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(NotificationDrawer);
+
