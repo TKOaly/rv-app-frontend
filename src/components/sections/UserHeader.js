76722c842a3a006532b461c620ea5bc0db6193a4
alehuo <aleksi.huotala@helsinki.fi>
2019-10-30 10:45:34 +0200

    Switch to function component & implement Redux functionality using hooks

--- src/components/sections/UserHeader.js
+++ src/components/sections/UserHeader.js
@@ -1,7 +1,7 @@
 import './styles/Header.scss';
-import { connect } from 'react-redux';
 import { doLogout } from '../../reducers/authenticationReducer';
 import { push } from 'connected-react-router';
+import { useDispatch, useSelector } from 'react-redux';
 import FontAwesome from 'react-fontawesome';
 import HeaderBtn from '../buttons/HeaderBtn';
 import Logo from '../../images/tkoaly2.svg';
@@ -9,56 +9,43 @@ import Margin from '../helpers/Margin';
 import React from 'react';
 import moneyFormatter from '../../services/moneyFormatter';
 
-class Header extends React.Component {
-    handleBackClick = () => {
-        this.props.push('/');
-    };
+const Header = () => {
 
-    render = () => {
-        return (
-            <header>
-                <div className="header-title">
-                    <img src={Logo} alt="logo" />
-                    <h1>Ruokavälitys</h1>
-                </div>
-                <div className="header-right">
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn onClick={this.handleBackClick} hover>
-                            <FontAwesome name="arrow-left" /> Back to product page
-                        </HeaderBtn>
-                    </Margin>
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn fill>
-                            <FontAwesome name="user-circle" />{' '}
-                            <span>
-                                <b>{this.props.username}</b> {moneyFormatter.centsToString(this.props.moneyBalance)} €
-                            </span>
-                        </HeaderBtn>
-                    </Margin>
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn onClick={this.props.doLogout} hover>
-                            <FontAwesome name="sign-out" /> Log out (ENTER)
-                        </HeaderBtn>
-                    </Margin>
-                </div>
-            </header>
-        );
-    };
-}
+    const dispatch = useDispatch();
+    const username = useSelector((state) => state.user.username);
+    const moneyBalance = useSelector((state) => state.user.moneyBalance);
 
-const mapStateToProps = (state) => {
-    return {
-        username: state.user.username,
-        moneyBalance: state.user.moneyBalance
-    };
+    const handleBackClick = () => dispatch(push('/'));
+    const logout = () => dispatch(doLogout());
+
+    return (
+        <header>
+            <div className="header-title">
+                <img src={Logo} alt="logo" />
+                <h1>Ruokavälitys</h1>
+            </div>
+            <div className="header-right">
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn onClick={handleBackClick} hover>
+                        <FontAwesome name="arrow-left" /> Back to product page
+                    </HeaderBtn>
+                </Margin>
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn fill>
+                        <FontAwesome name="user-circle" />{' '}
+                        <span>
+                            <b>{username}</b> {moneyFormatter.centsToString(moneyBalance)} €
+                        </span>
+                    </HeaderBtn>
+                </Margin>
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn onClick={logout} hover>
+                        <FontAwesome name="sign-out" /> Log out (ENTER)
+                    </HeaderBtn>
+                </Margin>
+            </div>
+        </header>
+    );
 };
 
-const mapDispatchToProps = {
-    doLogout,
-    push
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Header);
+export default Header;
