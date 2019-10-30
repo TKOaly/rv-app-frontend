b027a56567cf185c4253c20630f5426043a9938e
alehuo <aleksi.huotala@helsinki.fi>
2019-10-30 10:42:13 +0200

    Add debounce to Header UI actions. Upgrade project dependencies.

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,8 +1,9 @@
 import './styles/Header.scss';
-import { connect } from 'react-redux';
+import { debounce } from 'lodash';
 import { doLogout } from '../../reducers/authenticationReducer';
 import { push } from 'connected-react-router';
 import { showModal } from '../../reducers/modalReducer';
+import { useDispatch, useSelector } from 'react-redux';
 import Deposit from '../modals/Deposit';
 import FontAwesome from 'react-fontawesome';
 import HeaderBtn from '../buttons/HeaderBtn';
@@ -11,69 +12,59 @@ import Margin from '../helpers/Margin';
 import React from 'react';
 import moneyFormatter from '../../services/moneyFormatter';
 
-export class Header extends React.Component {
-    handleDepositClick = (event) => {
-        event.preventDefault();
-        this.props.showModal(Deposit);
-    };
+const Header = (props) => {
 
-    handleUserClick = () => {
-        this.props.push('/user');
-    };
+    const dispatch = useDispatch();
+    const username = useSelector((state) => state.user.username);
+    const moneyBalance = useSelector((state) => state.user.moneyBalance);
 
-    render = () => {
-        const { username, moneyBalance, doLogout } = this.props;
-        return (
-            <header>
-                <div className="header-title">
-                    <img src={Logo} alt="logo" />
-                    <h1>Ruokavälitys</h1>
-                </div>
-                <div className="header-right">
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn onClick={this.handleDepositClick} hover className="depositBtn">
+    const handleDepositClick = debounce((event) => {
+        event.persist();
+        dispatch(showModal(Deposit));
+    }, 75, {leading:false, trailing:true});
+
+    const handleUserClick = debounce(() => {
+        dispatch(push('/user'));
+    }, 50, {leading:false, trailing:true});
+
+    const logout = debounce(() => {
+        dispatch(doLogout());
+    }, 50, {leading:false, trailing:true});
+
+    return (
+        <header>
+            <div className="header-title">
+                <img src={Logo} alt="logo" />
+                <h1>Ruokavälitys</h1>
+            </div>
+            <div className="header-right">
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn onClick={handleDepositClick} hover className="depositBtn">
                             Deposit (D)
-                        </HeaderBtn>
-                    </Margin>
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn onClick={this.handleUserClick} fill hover>
-                            <FontAwesome name="user-circle" />{' '}
-                            <span>
-                                <b className="user-username">{username || 'N/A'}</b>{' '}
-                                <span className="user-money">
-                                    <span className="user-money-value">
-                                        {moneyFormatter.centsToString(moneyBalance || 0)}
-                                    </span>{' '}
+                    </HeaderBtn>
+                </Margin>
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn onClick={handleUserClick} fill hover>
+                        <FontAwesome name="user-circle" />{' '}
+                        <span>
+                            <b className="user-username">{username || 'N/A'}</b>{' '}
+                            <span className="user-money">
+                                <span className="user-money-value">
+                                    {moneyFormatter.centsToString(moneyBalance || 0)}
+                                </span>{' '}
                                     €
-                                </span>
                             </span>
-                        </HeaderBtn>
-                    </Margin>
-                    <Margin margin={5} inlineBlock>
-                        <HeaderBtn onClick={doLogout} hover className="logoutBtn">
-                            <FontAwesome name="sign-out" /> Log out (ENTER)
-                        </HeaderBtn>
-                    </Margin>
-                </div>
-            </header>
-        );
-    };
-}
-
-const mapStateToProps = (state) => {
-    return {
-        username: state.user.username,
-        moneyBalance: state.user.moneyBalance
-    };
+                        </span>
+                    </HeaderBtn>
+                </Margin>
+                <Margin margin={5} inlineBlock>
+                    <HeaderBtn onClick={logout} hover className="logoutBtn">
+                        <FontAwesome name="sign-out" /> Log out (ENTER)
+                    </HeaderBtn>
+                </Margin>
+            </div>
+        </header>
+    );
 };
 
-const mapDispatchToProps = {
-    showModal,
-    doLogout,
-    push
-};
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Header);
+export default Header;
